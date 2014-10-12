fc7436508d67b7579a8a2737e0841e6c86e0d9a0
Samurai <samurai@samurai-wtf.localhost>
2014-10-12 11:45:33 +0200

    c project

---
+++ banking_transactionsparser/Debug/src/banking_parser.d
@@ -0,0 +1,8 @@
+src/banking_parser.d: ../src/banking_parser.c ../src/bankingtypes.h \
+ ../src/utils.h ../src/banking_transactionprocessor.h
+
+../src/bankingtypes.h:
+
+../src/utils.h:
+
+../src/banking_transactionprocessor.h:
