7c5c9978099ff5d47c44a481e6a4627622103e42
Alba <alba.xhani@gmail.com>
2014-12-02 00:57:26 +0100

    batch upload interface and file path view

--- SCS_Team16/src/ui/BatchUploadView.java
+++ SCS_Team16/src/ui/BatchUploadView.java
@@ -11,64 +11,70 @@ import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-import tan.BatchGeneratorImp;
-import tan.IBatchGenerator;
-
-public class BatchUploadView extends UIView implements  ActionListener {
+public class BatchUploadView extends UIView implements ActionListener {
 
 	File file;
 	JFileChooser fc;
 	JButton openButton, tanGeneration;
-	JLabel fileLabel;
-	
+	JLabel fileLabel, tansFileLabel, userLabel;
+	IBatchUpload bachTanController;
+
 	public BatchUploadView(JPanel panel) {
 		super(panel);
 		panel.setLayout(null);
 
-		JLabel userLabel = new JLabel("Please choose the batch file");
+		userLabel = new JLabel("Please choose the batch file");
 		userLabel.setBounds(90, 10, 250, 25);
 		panel.add(userLabel);
-		
+
 		fc = new JFileChooser();
 		// Add listener on chooser to detect changes to selected file
 		fc.addPropertyChangeListener(new PropertyChangeListener() {
-		    public void propertyChange(PropertyChangeEvent evt) {
-		        if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY
-		                .equals(evt.getPropertyName())) {
-		         
+			public void propertyChange(PropertyChangeEvent evt) {
+				if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(evt
+						.getPropertyName())) {
 
-		            // The selected file should always be the same as newFile
-		
-		        } else if (JFileChooser.SELECTED_FILES_CHANGED_PROPERTY.equals(
-		                evt.getPropertyName())) {
-		        
-		            // Get list of selected files
-		            // The selected files should always be the same as newFiles
-		     
-		        }
-		    }
+					// The selected file should always be the same as newFile
+
+				} else if (JFileChooser.SELECTED_FILES_CHANGED_PROPERTY
+						.equals(evt.getPropertyName())) {
+
+					// Get list of selected files
+					// The selected files should always be the same as newFiles
+
+				}
+			}
 		});
+		// upload only .txt files
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"TEXT FILES", "txt", "text");
+		fc.setFileFilter(filter);
 		panel.add(fc);
 		JButton openButton = new JButton("Open a File...");
 		openButton.setBounds(200, 50, 150, 25);
 		openButton.addActionListener(this);
 		panel.add(openButton);
-		
+
 		fileLabel = new JLabel();
-        fileLabel.setBounds(50, 50, 250, 25);
-        fileLabel.setVisible(false);
+		fileLabel.setBounds(50, 50, 250, 25);
+		fileLabel.setVisible(false);
 		panel.add(fileLabel);
-		
-		//Tan generation button
+
+		// Tan generation button
 		tanGeneration = new JButton("Generate Tans");
 		tanGeneration.setBounds(120, 100, 150, 25);
 		tanGeneration.addActionListener(this);
 		panel.add(tanGeneration);
 		tanGeneration.setVisible(false);
-		
+
+		// Path for the created file with tans
+		tansFileLabel = new JLabel();
+		tansFileLabel.setBounds(100, 100, 250, 25);
+		tansFileLabel.setVisible(false);
+		panel.add(tansFileLabel);
+
 	}
 
 	@Override
@@ -76,22 +82,35 @@ public class BatchUploadView extends UIView implements  ActionListener {
 		String buttonText = ((JButton) arg0.getSource()).getText();
 		switch (buttonText) {
 		case "Open a File...":
-				//this.hideMe(true);
-				int returnVal = fc.showDialog(panel, "Upload");
-			      if (returnVal == JFileChooser.APPROVE_OPTION) {
-			        file = fc.getSelectedFile();
-			        fileLabel.setText(fc.getSelectedFile().getName());
-			        fileLabel.setVisible(true);
-			        tanGeneration.setVisible(true);
-			      }
-			break;   
+			// this.hideMe(true);
+			int returnVal = fc.showDialog(panel, "Upload");
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				file = fc.getSelectedFile();
+				fileLabel.setText(fc.getSelectedFile().getName());
+				fileLabel.setVisible(true);
+				tanGeneration.setVisible(true);
+			}
+			break;
 		case "Generate Tans":
-			//this.hideMe(true);
-			IBatchGenerator batchGenerator = new BatchGeneratorImp(file);
-	
-		break;  
+			// this.hideMe(true);
+
+			if (file.isFile()) {
+				panel.repaint();
+				bachTanController = new BatchUploadImp(file);
+				fileLabel.setVisible(false);
+				tanGeneration.setVisible(false);
+				userLabel.setVisible(false);
+				tansFileLabel.setText("Please find your tans in: "
+						+ bachTanController.generateTansFile());
+				tansFileLabel.setVisible(true);
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Invalid file selected!");
+			}
+
+			break;
 
 		}
 	}
-	
+
 }
