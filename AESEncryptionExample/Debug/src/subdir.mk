59ab15aa0714ce23a8dc312286b7dde0ce9f1327
mersad00 <mersad00@github.com>
2015-01-24 00:42:16 +0100

    c tan decryptor fully implemented

--- AESEncryptionExample/Debug/src/subdir.mk
+++ AESEncryptionExample/Debug/src/subdir.mk
@@ -4,20 +4,29 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/AESEncryptionExample.c 
+../src/AESEncryptionExample.c \
+../src/cdecode.c \
+../src/cencode.c \
+../src/sha256.c 
 
 OBJS += \
-./src/AESEncryptionExample.o 
+./src/AESEncryptionExample.o \
+./src/cdecode.o \
+./src/cencode.o \
+./src/sha256.o 
 
 C_DEPS += \
-./src/AESEncryptionExample.d 
+./src/AESEncryptionExample.d \
+./src/cdecode.d \
+./src/cencode.d \
+./src/sha256.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/home/samurai/Downloads/libb64-1.2.1/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- AESEncryptionExample/src/AESEncryptionExample.c
+++ AESEncryptionExample/src/AESEncryptionExample.c
@@ -8,7 +8,6 @@
  ============================================================================
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,77 +16,153 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <mutils/mcrypt.h>
-//int main(void) {
-//	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-//	return EXIT_SUCCESS;
-//}
+#include "cencode.h"
+#include "cdecode.h"
+#include "sha256.h"
 
+/* arbitrary buffer size */
+#define SIZE 100
 
-int encrypt(
-    void* buffer,
-    int buffer_len, /* Because the plaintext could include null bytes*/
-    char* IV,
-    char* key,
-    int key_len
-){
-  MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
-  int blocksize = mcrypt_enc_get_block_size(td);
-  if( buffer_len % blocksize != 0 ){return 1;}
+char* encode(const char* input) {
+	/* set up a destination buffer large enough to hold the encoded data */
+	char* output = (char*) malloc(SIZE);
+	/* keep track of our encoded position */
+	char* c = output;
+	/* store the number of bytes encoded by a single call */
+	int cnt = 0;
+	/* we need an encoder state */
+	base64_encodestate s;
 
-  mcrypt_generic_init(td, key, key_len, IV);
-  mcrypt_generic(td, buffer, buffer_len);
-  mcrypt_generic_deinit (td);
-  mcrypt_module_close(td);
+	/*---------- START ENCODING ----------*/
+	/* initialise the encoder state */
+	base64_init_encodestate(&s);
+	/* gather data from the input and send it to the output */
+	cnt = base64_encode_block(input, strlen(input), c, &s);
+	c += cnt;
+	/* since we have encoded the entire input string, we know that
+	 there is no more input data; finalise the encoding */
+	cnt = base64_encode_blockend(c, &s);
+	c += cnt;
+	/*---------- STOP ENCODING  ----------*/
 
-  return 0;
+	/* we want to print the encoded data, so null-terminate it: */
+	*c = 0;
+
+	return output;
 }
 
-int decrypt(
-    void* buffer,
-    int buffer_len,
-    char* IV,
-    char* key,
-    int key_len
-){
-  MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
-  int blocksize = mcrypt_enc_get_block_size(td);
-  if( buffer_len % blocksize != 0 ){return 1;}
+char* decode(const char* input) {
+	/* set up a destination buffer large enough to hold the encoded data */
+	char* output = (char*) malloc(SIZE);
+	/* keep track of our decoded position */
+	char* c = output;
+	/* store the number of bytes decoded by a single call */
+	int cnt = 0;
+	/* we need a decoder state */
+	base64_decodestate s;
 
-  mcrypt_generic_init(td, key, key_len, IV);
-  mdecrypt_generic(td, buffer, buffer_len);
-  mcrypt_generic_deinit (td);
-  mcrypt_module_close(td);
+	/*---------- START DECODING ----------*/
+	/* initialise the decoder state */
+	base64_init_decodestate(&s);
+	/* decode the input data */
+	cnt = base64_decode_block(input, strlen(input), c, &s);
+	c += cnt;
+	/* note: there is no base64_decode_blockend! */
+	/*---------- STOP DECODING  ----------*/
 
-  return 0;
+	/* we want to print the decoded data, so null-terminate it: */
+	*c = 0;
+
+	return output;
 }
 
-void display(char* ciphertext, int len){
-  int v;
-  for (v=0; v<len; v++){
-    printf("%d ", ciphertext[v]);
-  }
-  printf("\n");
+
+int encrypt(void* buffer, int buffer_len, /* Because the plaintext could include null bytes*/
+char* IV, char* key, int key_len) {
+	MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	int blocksize = mcrypt_enc_get_block_size(td);
+	if (buffer_len % blocksize != 0) {
+		return 1;
+	}
+
+	mcrypt_generic_init(td, key, key_len, IV);
+	mcrypt_generic(td, buffer, buffer_len);
+	mcrypt_generic_deinit(td);
+	mcrypt_module_close(td);
+
+	return 0;
 }
 
-int main()
-{
-  MCRYPT td, td2;
-  char * plaintext = "test text 123";
-  char* IV = "AAAAAAAAAAAAAAAA";
-  char *key = "0123456789abcdef";
-  int keysize = 16; /* 128 bits */
-  char* buffer;
-  int buffer_len = 16;
+int decrypt(void* buffer, int buffer_len, char* IV, char* key, int key_len) {
+	MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	int blocksize = mcrypt_enc_get_block_size(td);
+	if (buffer_len % blocksize != 0) {
+		return 1;
+	}
 
-  buffer = calloc(1, buffer_len);
-  strncpy(buffer, plaintext, buffer_len);
+	mcrypt_generic_init(td, key, key_len, IV);
+	mdecrypt_generic(td, buffer, buffer_len);
+	mcrypt_generic_deinit(td);
+	mcrypt_module_close(td);
 
-  printf("==C==\n");
-  printf("plain:   %s\n", plaintext);
-  encrypt(buffer, buffer_len, IV, key, keysize);
-  printf("cipher:  "); display(buffer , buffer_len);
-  decrypt(buffer, buffer_len, IV, key, keysize);
-  printf("decrypt: %s\n", buffer);
+	return 0;
+}
 
-  return 0;
+void display(char* ciphertext, int len) {
+	int v;
+	for (v = 0; v < len; v++) {
+		printf("%d ", ciphertext[v]);
+	}
+	printf("\n");
+}
+
+int main() {
+
+	//customer key get this from user table in DB
+	char* mykey = "5UvSoqvtVVtrV2ZW";
+
+	///here is the tan that you received
+	char * encryptedTan =
+			"FUrlvSwdV18pZI9fl/1ZXFT4dFu3CRkSvmbneNOg8lnFTwrjJlkaEiMjTrxEOn3pBEWQ1tDRYQ1rLvY1HeOLa4EGE/9R4YLtz+ZcJpSDWNUtpZk+/TkC9bH8AsBU7U+x";
+	/* decode the base64 */
+	char * decoded = decode(encryptedTan);
+
+	//take out iv and hmac and actual encrypted tan
+	//TODO: make sure the decoded array is large enough before substr
+	char* myiv = calloc(1, 16);
+	strncpy(myiv, decoded, 16);
+
+	char* myhmac = calloc(1, 32);
+	strncpy(myhmac, decoded + 16, 32);
+
+	int cryptlen = strlen(decoded) - 16 - 32;
+	char* mytan = calloc(1, strlen(decoded) - 16 - 32);
+	strncpy(mytan, decoded + 16 + 32, strlen(decoded) - 16 - 32);
+
+	//the actual used key is sha256 of the customer key
+	int mykeysize = 32;
+	char* mykeybyte = calloc(1, 32);
+	SHA256_CTX ctx;
+	sha256_init(&ctx);
+	sha256_update(&ctx, mykey, strlen(mykey));
+	sha256_final(&ctx, mykeybyte);
+
+	//let's decrypt the tan!
+	char* decipheredTan = calloc(1, cryptlen);
+	strncpy(decipheredTan, mytan, cryptlen);
+	decrypt(decipheredTan, cryptlen, myiv, mykeybyte, mykeysize);
+
+	//You can now pick up the decipheredTan
+	//TODO: trim character 14 at the end of decipheredTan
+	//Remember the timestamp is based on UTC
+	printf("decrypt: %s\n", decipheredTan);
+    //output: decrypt: 2;200;01/23/2015 10:57:08 PM +0100
+
+	free(decoded);
+	free(myiv);
+	free(myhmac);
+	free(mytan);
+	free(mykeybyte);
+	free(decipheredTan);
+	return 0;
 }
